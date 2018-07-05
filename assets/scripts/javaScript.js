16f4a01ecc8fb06a7f72bb9775fdda1988b86330
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2018-07-05 11:57:31 -0400

    logic phase one is done. Needs styling

--- assets/scripts/javaScript.js
+++ assets/scripts/javaScript.js
@@ -7,55 +7,67 @@ console.log("hello");
 // Creating variables to hold the number of wins, losses, and ties. They start at 0.
 var wins = 0;
 var losses = 0;
-var ties = 0;
+var draw = 0;
 
 // Create variables to grab and update the scores
 var userScore_span = document.getElementById("user-score");
 var computerScore_span = document.getElementById("comp-score");
+var result_div = document.getElementById("results");
 
 // Create variables to watch for the user's choice
-const rock_div = document.getElementById('r');
-const paper_div = document.getElementById('p');
-const scissor_div = document.getElementById('s');
+const rock_div = document.getElementById('rock');
+const paper_div = document.getElementById('paper');
+const scissor_div = document.getElementById('scissor');
 
 // 2. Make functions to operate the Game
 // ======================================
 // Get computer's  choice-  use Math.Floor to round random numbers down to the nearest whole number. use Math.Random to populate a random number between 0 and 3
 function makeComputerChoice() {
   // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-  const computerChoice = ["r", "p", "s"];
+  const computerChoice = ["rock", "paper", "scissor"];
   const randomNumber = Math.floor(Math.random()*3);
   return computerChoice[randomNumber];
 }
 
-function win() {
+function win(user, computer) {
   wins++;
-  userScore_span.innerHTML= wins
+  userScore_span.innerHTML= wins;
+  result_div.innerHTML= `${user.toUpperCase()} beats ${computer.toUpperCase()}.  You Win!`;
+  document.getElementById(user).classList.add('green-glow');
+  setTimeout(()=> document.getElementById(user).classList.remove('green-glow'), 300);
 }
 
-function lose() {
+function lose(user, computer) {
   losses++;
-  computerScore_span.innerHTML = losses
+  computerScore_span.innerHTML = losses;
+  result_div.innerHTML= `${computer.toUpperCase()} beats ${user.toUpperCase()}.  You Lose!`;
 }
+
+function ties() {
+  result_div.innerHTML= "Great minds think alike.  Let's play again.";
+}
+
 function game(userChoice) {
   const computerPick = makeComputerChoice();
   switch (userChoice + computerPick) {
-    case "rs":
-    case "pr":
-    case "sp":
-    win();
+    case "rockscissor":
+    case "paperrock":
+    case "scissorpaper":
+    win(userChoice, computerPick);
+    break;
 
-    break;
-    case "rp":
-    case "ps":
-    case "sr":
+    case "rockpaper":
+    case "paperscissor":
+    case "scissorrock":
     console.log("You Lose " + userChoice + computerPick);
-    lose();
+    lose(userChoice, computerPick);
     break;
-    case "rr":
-    case "pp":
-    case "ss":
+
+    case "rockrock":
+    case "paperpaper":
+    case "scissorscissor":
     console.log("We Tie " + userChoice + computerPick);
+    ties();
     break;
   }
 };
@@ -65,17 +77,11 @@ function game(userChoice) {
 function main() {
 // +++ Pull in the user's selection +++
 // send the letter r for the user's choice if they click on the rock picture
-rock_div.addEventListener("click", function() {
-  game("r");
-})
+rock_div.addEventListener("click", () => game("rock"));
 // send the letter p for the user's choice if they click on the paper picture
-paper_div.addEventListener("click", function() {
-  game("p");
-})
+paper_div.addEventListener("click", () => game("paper"));
 // send the letter s for the user's choice if they click on the scissor picture
-scissor_div.addEventListener("click", function() {
-  game("s");
-})
+scissor_div.addEventListener("click", () => game("scissor"));
 }
 
 main();
